3f3c07c60170a5afaf03cb644fcd482e8840811a
Eric Wu <eric@Eric-MBP.local>
2017-12-11 12:22:22 +0800

    modify read command

--- install_package.sh
+++ install_package.sh
@@ -7,10 +7,7 @@ yum install -y mesa-libEGL mesa-libGL libICE libSM libXext mtdev xcb-util-wm xcb
 wget https://minergate.com/download/rpm-cli -O minergate-cli.rpm
 rpm -ivh minergate-cli.rpm
 
-echo -n "請輸入您的minergate註冊的mail : "
-read mail
-echo -n "請輸入您要挖礦的幣別 : "
-read currency
-echo -n "請輸入您要分配的cpu core數 : "
-read core
+read -p  "請輸入您的minergate註冊的mail : " mail
+read -p  "請輸入您要挖礦的幣別 : " currency
+read -p  "請輸入您要分配的cpu core數 : " core
 nohup minergate-cli -u $mail --$currency $core &

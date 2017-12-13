fe4a50721230345687b8dd491b57377dd76b2c57
Eric Wu <eric@Eric-MBP.local>
2017-12-13 15:52:23 +0800

    modift install script

--- install_package.sh
+++ install_package.sh
@@ -6,7 +6,6 @@ yum install vim wget -y
 yum install -y mesa-libEGL mesa-libGL libICE libSM libXext mtdev xcb-util-wm xcb-util-image xcb-util-keysyms compat-libxcb libXrender fontconfig libfontconfig.so.1
 wget https://minergate.com/download/rpm-cli -O minergate-cli.rpm
 rpm -ivh minergate-cli.rpm
-
 read -p  "請輸入您的minergate註冊的mail : " mail
 read -p  "請輸入您要挖礦的幣別 : " currency
 read -p  "請輸入您要分配的cpu core數 : " core
